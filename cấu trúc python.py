df99bc7bd408fae6b97c6774ca21fbe3ecd15670
dohuudat311 <datdo1777@gmail.com>
2026-05-22 13:09:10 +0700

    Add files via upload

---
+++ cấu trúc python.py
@@ -0,0 +1,5 @@
+print(' Lập trình Python ')
+a = 100
+print(' Gtri : ', a)
+print(" i love python ")
+print(" Khoa trắc địa bản đồ và quản lí đất đai ")
---
+++ toán tử.py
@@ -0,0 +1,7 @@
+a = 10
+b = 2
+list_data = [1, 2, 3, 4, 5]
+print(a in list_data)
+print(b in list_data)
+print(a not in list_data)
+print(b not in list_data)
